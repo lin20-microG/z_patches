d60f063b92eec81c155a3c6107fdba6e199ece1a
mse1969 <mse1969@posteo.de>
2024-03-12 21:44:55 +0100

    build-script: add beyond1lte
    Change-Id: I39e716863638256ffc6dce94fe70b0f77e4de416

--- build_device.sh
+++ build_device.sh
@@ -3,7 +3,7 @@
 print_help() {
   echo "usage: build_device <device> test|sign"
   echo "----------------------------------------------------------------------"
-  echo " <device> Device name (hotdog|x86)"
+  echo " <device> Device name (beyond1lte|hotdog|x86)"
   echo "test - build with testkeys (insecure, but compatible)"
   echo "sign - create a signed build"
 }
@@ -14,7 +14,7 @@ print_device() {
 
 # Check parameters
 case "$1" in
-  hotdog|x86)
+  beyond1lte|hotdog|x86)
      print_device $1
     ;;
   *) print_help
